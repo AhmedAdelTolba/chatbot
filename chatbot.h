6badf534117fa03b99bb2fb7754d54ab40a5569b
AhmedAdelTolba <a7med.toulba@gmail.com>
2020-06-01 17:31:18 +0000

    Add files via upload

---
+++ chatbot.h
@@ -0,0 +1,44 @@
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <wx/bitmap.h>
+#include <string>
+
+class GraphNode; // forward declaration
+class ChatLogic; // forward declaration
+
+class ChatBot
+{
+private:
+    // data handles (owned)
+    wxBitmap *_image; // avatar image
+
+    // data handles (not owned)
+    GraphNode *_currentNode;
+    GraphNode *_rootNode;
+    ChatLogic *_chatLogic;
+
+    // proprietary functions
+    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+
+public:
+    // constructors / destructors
+    ChatBot();                     // constructor WITHOUT memory allocation
+    ChatBot(std::string filename); // constructor WITH memory allocation
+    ChatBot(const ChatBot& chatbot_copy); //copy constructor
+    ChatBot& operator= (const ChatBot& chatbot_copy);
+    ChatBot& operator= (ChatBot&& chatbot_copy);
+    ChatBot(ChatBot&& chatbot_copy); //move semantic
+    ~ChatBot();
+
+    // getters / setters
+    void SetCurrentNode(GraphNode *node);
+    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+    wxBitmap *GetImageHandle() { return _image; }
+
+    // communication
+    void ReceiveMessageFromUser(std::string message);
+};
+
+#endif /* CHATBOT_H_ */
